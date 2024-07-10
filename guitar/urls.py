74607b933de203650c8cc4913920740ac55e8780
Natasha Graham <natashasusangraham@ymail.com>
2024-07-10 23:15:42 +0100

    Category page

--- guitar/urls.py
+++ guitar/urls.py
@@ -6,7 +6,7 @@ app_name = 'guitar'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug/',
+    path('category/<slug:category_name_slug>/',
          views.show_category, name='show_category'),
     
 ]
